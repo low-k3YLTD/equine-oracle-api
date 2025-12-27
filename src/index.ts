59198d58aca3d08c93500870d0ea81a2d62bd929
low-k3YLTD <low.k3yltd@gmail.com>
2025-12-28 02:31:25 +1300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
- import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { authMiddleware } from './middleware/authMiddleware';
 import { getMlPredictions } from './services/mlPredictionService';
